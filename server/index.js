041f998f79ceaa91507ddde66b3f09c740129d70
WaelAlturi <alturiwael@gmail.com>
2025-05-25 04:28:03 +0300

    Ready For ServerSide

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("DB IS ON"))
+  .catch((e) => {
+    return e.message;
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Run On PORT:${process.env.PORT}`);
+});
